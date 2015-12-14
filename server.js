29ec6a388cd77c93bfb3f8885daf3e8a25794c3b
Jerry Yang Jin <jerry.yang.jin@gmail.com>
2015-12-14 15:15:58 +0800

    fb login

--- server.js
+++ server.js
@@ -7,9 +7,6 @@ var file = new static.Server('./');
 
 require('http').createServer(function (request, response) {
     request.addListener('end', function () {
-        //
-        // Serve files!
-        //
         file.serve(request, response);
     }).resume();
 }).listen(8080);
