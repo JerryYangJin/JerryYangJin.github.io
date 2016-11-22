c8d37fae8f09fd91ae292abb42e351a336ffa41d
Jerry Jin <jerry.yang.jin@gmail.com>
2016-11-22 20:02:37 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -8,12 +8,6 @@ angular.module('jin',['ui.router','angularVideoBg'])
   $urlRouterProvider.otherwise('/');
   $locationProvider.html5Mode(true);
 
-  $('#video').height($(window).innerHeight()+'px');
-
-  $(window).on('resize', function(){
-    $('#video').height($(window).innerHeight()+'px');
-  });
-
   $stateProvider
 
     .state('index', {
